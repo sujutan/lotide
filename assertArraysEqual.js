cffaa1c71067f2277fd78b485a77a278882a29a2
Suju Tan <sujuktan@gmail.com>
2023-04-25 16:53:07 -0400

    Added a function to compare two arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,32 @@
+// Check if two arrays are equal
+const eqArrays = function(array1, array2) {
+  // The two arrays have the same length
+  if(array1.length === array2.length) {
+    if(array1.length === 0){ // Two arrays are empty array
+      return true;
+    } else { // Check each item of the two arrays
+      for(let i = 0; i < array1.length; i++) {
+        if(array1[i] !== array2[i]){
+          return false;
+        }
+      }
+      return true;
+    }
+  } else { // The two arrays have different lengths
+    return false;
+  }
+}
+
+// Assertion function
+const assertArraysEqual = function(array1, array2) {
+  if(eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`);
+  }
+}
+
+// Test cases
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]);
+assertArraysEqual([], []);
+assertArraysEqual(["lighthouse", "LHL"], ["lighthouse"]);
