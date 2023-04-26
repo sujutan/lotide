684224f15602724adf4ad9071ebf394b12adcbca
Suju Tan <sujuktan@gmail.com>
2023-04-25 22:01:49 -0400

    Added a function to return the middle-most element(s) of the given array

---
+++ middle.js
@@ -0,0 +1,50 @@
+// Check if two arrays are equal
+const eqArrays = function(array1, array2) {
+  // The two arrays have the same length
+  if(array1.length === array2.length) {
+    if(array1.length === 0){ // Two arrays are empty array
+      return true;
+    } else { // Check each item of the two arrays
+      for(let i = 0; i < array1.length; i++) {
+        if(array1[i] !== array2[i]){
+          return false;
+        }
+      }
+      return true;
+    }
+  } else { // The two arrays have different lengths
+    return false;
+  }
+}
+
+// Assertion function
+const assertArraysEqual = function(array1, array2) {
+  if(eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`);
+  }
+}
+
+const middle = function(array) {
+  let result = [];
+  if(array.length > 2) {
+    if(array.length % 2 === 1) { // Odd elements
+      result.push(array[Math.floor(array.length / 2)]);
+    } else { // Even elements
+      result.push(array[array.length / 2 - 1]);
+      result.push(array[array.length / 2]);
+    }
+  }
+  return result;
+}
+
+// Test code
+assertArraysEqual(middle([1]), []); // => []
+assertArraysEqual(middle([1, 2]), []); // => []
+
+assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
+
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
